f9caa51e85ac3192a1344b118e50d9adf3609598
zhaojh0912 <1763124707@qq.com>
2021-02-02 17:08:53 +0800

    2021-02-02 提交日志  提交人：zhaojh   提交内容：   1、提交 springcloud-alibaba-all 的基础 框架

---
+++ springcloud-alibaba-user/src/main/java/com/example/user/SpringcloudAlibabaUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAlibabaUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudAlibabaUserApplication.class, args);
+	}
+
+}
